8ccd3ab32fe8cee7efab8e4cf1b17aef5972fa57
HridyThug <144492086+HridyThug@users.noreply.github.com>
2026-03-13 01:16:54 +0530

    First Commit

---
+++ port/js/script.js
@@ -0,0 +1,144 @@
+/* ================================= typing animation ======================== */
+let words=document.querySelectorAll(".word");
+words.forEach((word)=>{
+    let letters = word.textContent.split("");
+    word.textContent="";
+    letters.forEach((letter)=>{
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+    });
+});
+
+let currentWordIndex=0;
+let maxWordIndex= words.length -1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText = ()=>{
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter,i)=>{
+        setTimeout(()=>{
+            letter.className = "letter out";
+        },i * 80);
+    });
+    nextWord.style.opacity="1";
+    Array.from(nextWord.children).forEach((letter,i)=>{
+        letter.className="letter behind";
+        setTimeout(()=>{
+            letter.className = "letter in";
+        },340 + i * 80);
+    });
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+
+changeText();
+setInterval(changeText,3000);
+/*=================== Aside ================================================ */
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+    for(let i=0; i<totalNavList; i++)
+    {
+        const a = navList[i].querySelector("a");
+        a.addEventListener("click",function()
+        {
+            removeBackSection();
+            for(let j=0; j<totalNavList; j++)
+            {
+                if(navList[j].querySelector("a").classList.contains("active"))
+                {
+                    addBackSection(j);
+                    //allSection[j].classList.add("back-section");
+                }
+                navList[j].querySelector("a").classList.remove("active");
+            }
+            this.classList.add("active")
+            showSection(this);
+            if(window.innerWidth < 1200)
+            {
+                asideSectionTogglerBtn();
+            }
+        })
+    }
+    function removeBackSection()
+    {
+        for(let i=0; i<totalSection; i++)
+        {
+            allSection[i].classList.remove("back-section");
+        }
+    }
+    function addBackSection(num)
+    {
+        allSection[num].classList.add("back-section");
+    }
+    function showSection(element)
+    {
+        for(let i=0; i<totalSection; i++)
+        {
+            allSection[i].classList.remove("active");
+        }
+        const target = element.getAttribute("href").split("#")[1];
+        document.querySelector("#" + target).classList.add("active")
+    }
+
+    function updateNav(element)
+    {
+        for(let i=0; i<totalNavList; i++)
+        {
+            navList[i].querySelector("a").classList.remove("active");
+            const target = element.getAttribute("href").split("#")[1];
+            if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1])
+            {
+                navList[i].querySelector("a").classList.add("active");
+            }
+        }
+    }
+    document.querySelector(".hire-me").addEventListener("click",function()
+    {
+        const sectionIndex = this.getAttribute("data-section-index");
+        //console.log(sectionIndex)
+        showSection(this);
+        updateNav(this);
+        removeBackSection();
+        addBackSection(sectionIndex);
+    })
+    const navTogglerBtn = document.querySelector(".nav-toggler"),
+            aside = document.querySelector(".aside");
+            navTogglerBtn.addEventListener("click",()=>
+            {
+                asideSectionTogglerBtn();
+            })
+            function asideSectionTogglerBtn()
+            {
+                aside.classList.toggle("open");
+                navTogglerBtn.classList.toggle("open");
+                for(let i=0; i<totalSection; i++)
+                {
+                    allSection[i].classList.toggle("open");
+                }
+            }
+
+
+const circle = document.querySelectorAll('.circle');
+circle.forEach(elem=>{
+        var dots=elem.getAttribute("data-dots");
+        var marked= elem.getAttribute("data-percent");
+        var percent = Math.floor(dots*marked/100);
+        var points = "";
+        var rotate = 360/dots;
+            
+        for(let i=0 ; i<dots ;i++){
+            points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+        }
+        elem.innerHTML = points;
+    
+    const pointsMarked = elem.querySelectorAll('.points');
+    for(let i=0; i<percent; i++){
+        pointsMarked[i].classList.add('marked')
+    }
+})
